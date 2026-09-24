c05a63e5baf4a4f227bbb6ea7a2cad90a9c14d3c
Geemanthi <pigera.geemanthi@gmail.com>
2026-09-24 13:51:55 +1000

    add alb security group

---
+++ infrastructure/security-groups.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "alb" {
+  name        = "sg-${local.name_suffix}-alb"
+  description = "Allow inbound HTTP from the internet"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge({
+    Name = "sg-${local.name_suffix}-alb"
+  }, local.tags)
+}
