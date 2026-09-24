41c1c9a6557d7e7e59b7ff76e2be64ec1bcdfc7b
Geemanthi <pigera.geemanthi@gmail.com>
2026-09-24 12:58:52 +1000

    plan vpc only

--- infrastructure/dev.tfvars
+++ infrastructure/dev.tfvars
@@ -1,1 +1,4 @@
-
+region_code = "nv"
+organization = "global"
+project = "webapp"
+vpc_cidr_block = "10.0.8.0/22"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,23 @@ variable "env" {
     type        = string
     description = "The environment to deploy"
 }
+
+variable "vpc_cidr_block" {
+    type        = string
+    description = "The CIDR block for the VPC"
+}
+
+variable "region_code" {
+    type        = string
+    description = "The AWS region code"
+}
+
+variable "organization" {
+    type        = string
+    description = "The organization name"
+}
+
+variable "project" {
+    type        = string
+    description = "The project name"
+}
